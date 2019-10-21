c1a22cb7653fd31784ccc233b941ecd9df566b78
IbtihelSidhom <ibtihel.sidhom@ensi-uma.tn>
2019-10-21 04:21:40 +0200

    step-1 - Define owners model.

--- carOwners/owners/models.py
+++ carOwners/owners/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Owner(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=200)
+    car_model = models.CharField(max_length=200)
+    car_model_year = models.IntegerField()
+    car_vin = models.CharField(max_length=200)
+    job_title = models.CharField(max_length=200)
+    country = models.CharField(max_length=200)
+    class Meta:
+        db_table = 'owners'
+
+    def __str__(self):
+        return self.first_name
